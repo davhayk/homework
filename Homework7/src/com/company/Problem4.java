3c331cbd70fe0fd90d188a79d24f411bb379e5a2
davhayk <davtyanh2003@gmail.com>
2020-03-10 14:55:40 +0400

    Edit Problem4

--- Homework7/src/com/company/Problem4.java
+++ Homework7/src/com/company/Problem4.java
@@ -5,16 +5,18 @@ import java.util.Arrays;
 public class Problem4 {
     public static void main(String[] args) {
         String[] stringArr = new String[] {"acd", "katu", "shun", "ads", "wad"};
-        System.out.println("The modified array is: " + modifier(stringArr));
+        System.out.println("The modified array is: " + Arrays.toString(modifier(stringArr)));
     }
     static String[] modifier (String[] stringArr) {
-        char[] temp = new char[100];
-        for (int i = 0; i < stringArr.length-1; i++) {
-            for (int j = stringArr[i].length()-1, k = 0; j > k; j--, k++) {
-                temp = stringArr[i].toCharArray();
+        char tempChar;
+        for (int i = 0; i < stringArr.length; i++) {
+            char[] temp = stringArr[i].toCharArray();
+            for (int j = stringArr[i].length()-1, k = 0; j >= k; j--, k++) {
+                tempChar = temp[k];
                 temp[k] = temp[j];
+                temp[j] = tempChar;
             }
-            stringArr[i] = Arrays.toString(temp);
+            stringArr[i] = new String(temp);
         }
         return stringArr;
     }
